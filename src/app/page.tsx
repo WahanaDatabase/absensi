b753431d360164f7f56dc90c858bdb6e697e048f
iqbal <ifachry@rocketmail.com>
2024-06-21 18:35:07 +0700

    dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="bg-[url('/Login.png')] text-white">
+    <main className="bg-[url('/Login.png')] bg-cover  h-screen max-w-full text-white">
       <div>Login</div>
     </main>
   );
