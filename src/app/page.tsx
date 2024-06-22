22fb647fb7ed20a32aafef0b3d434b38a18bd4b9
iqbal <ifachry@rocketmail.com>
2024-06-23 03:30:21 +0700

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { SignIn } from "@/components/sign-in";
+
 export default function Home() {
   return (
     <main className="bg-[url('/Login.png')] bg-cover  h-screen max-w-full text-white">
       <div>Login</div>
+      <SignIn />
     </main>
   );
 }
