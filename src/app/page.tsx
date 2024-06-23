03b3efcc9bdecd955fff00b9451383ad42e8071f
iqbal <ifachry@rocketmail.com>
2024-06-23 17:28:13 +0700

    fix login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import { SignIn } from "@/components/sign-in";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-login bg-cover  h-screen max-w-full text-white">
-      <div>PT. WAHANAKARSA SWANDIRI</div>
+    <main className="bg-login bg-cover h-screen max-w-full text-white flex flex-col justify-center items-center">
+      <div className="w-[247px] h-[229px] relative">
+        <Image src="/img/logo-sm.png" fill alt="" />
+      </div>
+      <div className="text-center text-[33px] font-semibold mb-2">
+        PT. WAHANAKARSA SWANDIRI
+      </div>
+      <div className="border-b border-2 w-[484px]" />
+      <div className="text-center text-[29px] font-semibold mt-2 mb-8">
+        CONSTRUCTION COMPANY
+      </div>
 
       <SignIn />
     </main>
--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -12,43 +12,28 @@ export function SignIn() {
   return (
     <>
       <form action={formAction}>
-        <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-          <h1 className={` mb-3 text-2xl text-black`}>
-            Please log in to continue.
-          </h1>
-          <div className="w-full">
+        <div className="flex-1   rounded-lg px-6 pb-4 pt-8">
+          <div className="w-[412px]">
             <div>
-              <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                htmlFor="email"
-              >
-                Email
-              </label>
               <div className="relative  text-black">
                 <input
                   className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                   id="email"
                   type="email"
                   name="email"
-                  placeholder="Enter your email address"
+                  placeholder="EMAIL"
                   required
                 />
               </div>
             </div>
             <div className="mt-4">
-              <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                htmlFor="password"
-              >
-                Password
-              </label>
               <div className="relative text-black">
                 <input
                   className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                   id="password"
                   type="password"
                   name="password"
-                  placeholder="Enter password"
+                  placeholder="PASSWORD"
                   required
                   minLength={3}
                 />
@@ -58,7 +43,9 @@ export function SignIn() {
               </div>
             </div>
           </div>
-          <button className="mt-4 w-full text-black">Log in</button>
+          <button className="mt-4 w-[412px] mx-auto bg-white flex items-center h-[62px] justify-center">
+            <p className=" text-[#2148C0]">LOGIN</p>
+          </button>
         </div>
       </form>
     </>
