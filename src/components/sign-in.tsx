a0eefb19f998820eabba3dfd3e6e9562c46e9141
iqbal <ifachry@rocketmail.com>
2024-06-23 21:35:13 +0700

    fix login

--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -1,53 +1,16 @@
-"use client";
-import { authenticate } from "@/lib/actions";
+import { getSession } from "@/lib/getSession";
+import { redirect } from "next/navigation";
 
-import { useFormState } from "react-dom";
+import Form from "./form";
 
-export function SignIn() {
-  const initialState = {
-    msg: "",
-  };
-  const [state, formAction] = useFormState(authenticate, initialState);
+export async function SignIn() {
+  const session = await getSession();
+  const user = session?.user;
+  if (user) redirect("/dashboard");
 
   return (
     <>
-      <form action={formAction}>
-        <div className="flex-1   rounded-lg px-6 pb-4 pt-8">
-          <div className="w-[412px]">
-            <div>
-              <div className="relative  text-black">
-                <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="EMAIL"
-                  required
-                />
-              </div>
-            </div>
-            <div className="mt-4">
-              <div className="relative text-black">
-                <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  id="password"
-                  type="password"
-                  name="password"
-                  placeholder="PASSWORD"
-                  required
-                  minLength={3}
-                />
-                <p aria-live="polite" className="text-12 text-red-500">
-                  {state?.msg}
-                </p>
-              </div>
-            </div>
-          </div>
-          <button className="mt-4 w-[412px] mx-auto bg-white flex items-center h-[62px] justify-center">
-            <p className=" text-[#2148C0]">LOGIN</p>
-          </button>
-        </div>
-      </form>
+      <Form />
     </>
   );
 }
