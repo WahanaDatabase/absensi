8413623a79dfd6dc2d75534106109df1a4f43446
iqbal <ifachry@rocketmail.com>
2024-07-26 15:17:12 +0700

    fix

--- src/components/dashboard/laporan-absensi/Absensi.tsx
+++ src/components/dashboard/laporan-absensi/Absensi.tsx
@@ -11,7 +11,6 @@ function Absensi({ attendances }: any) {
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState("");
   const [sortOrder, setSortOrder] = useState("Newest");
-
   const handleRowClick = (id: any) => {
     router.push(`/dashboard/data-karyawan/${id}`);
   };
@@ -31,14 +30,14 @@ function Absensi({ attendances }: any) {
   };
 
   // Filter users based on search term
-  const filteredUsers = attendances.filter((user: any) =>
-    user.Employee.name.toLowerCase().includes(searchTerm.toLowerCase())
+  const filteredUsers = attendances.filter((user: Attendance) =>
+    user.status.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   // Sort users based on sortOrder
   const sortedUsers = filteredUsers.sort((a: Attendance, b: Attendance) => {
     if (sortOrder === "Newest") {
-      return Number(new Date(b.tanggal)) - Number(new Date(a.tanggal));
+      return Number(new Date(b.tanggal)) - Number(new Date(a.tanggal)); // Replace 'creationDate' with your date field
     } else {
       return Number(new Date(a.tanggal)) - Number(new Date(b.tanggal));
     }
@@ -71,25 +70,16 @@ function Absensi({ attendances }: any) {
 
   const [selectedMonth, setSelectedMonth] = useState("");
   const [selectedYear, setSelectedYear] = useState("");
-
-  // Count attendance statuses for each employee
-  const statusCounts = attendances.reduce((acc: any, attendance: any) => {
-    if (!acc[attendance.Employee.id]) {
-      acc[attendance.Employee.id] = {
-        Hadir: 0,
-        Sakit: 0,
-        Alpha: 0,
-        Telat: 0,
-        Missed: 0,
-      };
-    }
-    acc[attendance.Employee.id][attendance.status]++;
-    return acc;
-  }, {});
-
+  const statusCounts = attendances.reduce(
+    (acc: any, attendance: Attendance) => {
+      acc[attendance.status] = (acc[attendance.status] || 0) + 1;
+      return acc;
+    },
+    {}
+  );
   return (
-    <div className="mb-10 flex flex-col">
-      <div className="mt-[12px] bg-white flex w-[968px] px-12 py-3 mx-auto rounded-[30px] shadow-sm justify-between">
+    <div className="mb-10 flex flex-col ">
+      <div className="mt-[12px]  bg-white flex w-[968px] px-12 py-3 mx-auto rounded-[30px] shadow-sm justify-between">
         <div className="flex space-x-8">
           <div className="bg-white justify-center flex items-center">
             <div className="text-[#ACACAC] text-[14px] font-normal">Bulan</div>
@@ -128,17 +118,18 @@ function Absensi({ attendances }: any) {
         </div>
 
         <div className="flex space-x-8 items-center justify-center">
-          <div className="ml-[71px] rounded-[4px] flex items-center justify-center bg-[#16C098]/[38%] w-[180px] h-[36px]">
+          <div className=" ml-[71px] rounded-[4px] flex items-center justify-center bg-[#16C098]/[38%] w-[180px] h-[36px]">
             <Link
               href={`/dashboard/registrasi-karyawan`}
-              className="text-[#008767] text-[14px]"
+              className=" text-[#008767]   text-[14px]"
             >
+              {" "}
               Download
             </Link>
           </div>
         </div>
       </div>
-      <div className="w-[968px] mt-[15px] shadow-sm ml-[71px] rounded-[30px] bg-white p-7">
+      <div className=" w-[968px] mt-[15px] shadow-sm  ml-[71px] rounded-[30px] bg-white p-7">
         <div className="flex justify-between items-center mb-4">
           <ShowEntries
             entriesToShow={entriesToShow}
@@ -178,14 +169,10 @@ function Absensi({ attendances }: any) {
           </thead>
           <tbody>
             {paginatedUsers.map((employee: any, index: any) => (
-              <tr
-                key={index}
-                onClick={() => handleRowClick(employee.id)}
-                className="cursor-pointer"
-              >
+              <tr key={index}>
                 <td className="border px-4 py-2">{startIndex + index + 1}</td>
                 <td className="border px-4 py-2">{employee.Employee.name}</td>
-                <td className="border px-4 py-2">{employee.Employee.id}</td>
+                <td className="border px-4 py-2">{employee.id}</td>
                 <td className="border px-4 py-2">{employee.Employee.group}</td>
                 <td className="border px-4 py-2">
                   {employee.status === "HADIR" ? 1 : 0}
@@ -211,10 +198,7 @@ function Absensi({ attendances }: any) {
           </tbody>
         </table>
         <div className="flex justify-between items-center mt-4">
-          <div>
-            Showing data {startIndex + 1} to {endIndex} of {sortedUsers.length}{" "}
-            entries
-          </div>
+          <div>Showing data 1 to 8 of 256K entries</div>
           <div className="flex">
             {Array.from({ length: totalPages }, (_, index) => (
               <button
