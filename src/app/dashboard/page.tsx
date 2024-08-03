afdc8517f9d08b7eeea227ecffa9b1e3b183a7e1
iqbal <ifachry@rocketmail.com>
2024-08-03 10:00:08 +0700

    fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -14,44 +14,44 @@ async function page() {
   return (
     <div className="flex-1 bg-[#FAFBFF]">
       <div className="text-[24px] p-8 pt-[41px] pl-[71px]">Main Dashboard</div>
-      <div className="mt-[37px] bg-white flex flex-col lg:flex-row w-full lg:w-[968px] px-6 lg:px-12 py-8 mx-auto rounded-[30px] shadow-sm justify-between space-y-6 lg:space-y-0">
-        <div className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-8">
-          <div className="w-[84px] h-[84px] relative mx-auto sm:mx-0">
+      <div className="mt-[37px] bg-white flex w-[968px] px-12 py-8 mx-auto rounded-[30px] shadow-sm justify-between">
+        <div className="flex space-x-8">
+          <div className="w-[84px] h-[84px] relative ">
             <Image src="/group.png" fill alt="karyawan" />
           </div>
-          <div className="bg-white justify-center flex flex-col text-center sm:text-left">
+          <div className=" bg-white  justify-center flex flex-col ">
             <div className="text-[#ACACAC] text-[14px] font-normal">
               Jumlah Karyawan
             </div>
-            <div className="flex items-start justify-center sm:justify-start text-[32px] font-semibold">
+            <div className="flex items-start text-[32px] font-semibold">
               {jumlahKaryawan}
             </div>
           </div>
-          <div className="border-l border-[#F0F0F0] hidden sm:block sm:pl-10"></div>
+          <div className=" border-l border-[#F0F0F0] pl-10"></div>
         </div>
-        <div className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-8">
-          <div className="w-[84px] h-[84px] relative mx-auto sm:mx-0">
+        <div className="flex space-x-8">
+          <div className="w-[84px] h-[84px] relative ">
             <Image src="/group.png" fill alt="karyawan" />
           </div>
-          <div className="bg-white justify-center flex flex-col text-center sm:text-left">
+          <div className=" bg-white  justify-center flex flex-col ">
             <div className="text-[#ACACAC] text-[14px] font-normal">
               Jumlah Lokasi
             </div>
-            <div className="flex items-start justify-center sm:justify-start text-[32px] font-semibold">
+            <div className="flex items-start text-[32px] font-semibold">
               {jumlahLokasi}
             </div>
           </div>
-          <div className="border-l border-[#F0F0F0] hidden sm:block sm:pl-10"></div>
+          <div className=" border-l border-[#F0F0F0] pl-10"></div>
         </div>
-        <div className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-8">
-          <div className="w-[84px] h-[84px] relative mx-auto sm:mx-0">
+        <div className="flex space-x-8">
+          <div className="w-[84px] h-[84px] relative ">
             <Image src="/group.png" fill alt="karyawan" />
           </div>
-          <div className="bg-white justify-center flex flex-col text-center sm:text-left">
+          <div className=" bg-white justify-center flex flex-col ">
             <div className="text-[#ACACAC] text-[14px] font-normal">
               Jumlah Absensi
             </div>
-            <div className="flex items-start justify-center sm:justify-start text-[32px] font-semibold">
+            <div className="flex items-start text-[32px] font-semibold">
               {jumlahAbsensi}
             </div>
           </div>
