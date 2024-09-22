8adb51a10b44d720230512f6492d6306e1d8224f
iqbal <ifachry@rocketmail.com>
2024-09-22 08:17:43 +0700

    fix

--- src/lib/getUserFromDbByEmail.ts
+++ src/lib/getUserFromDbByEmail.ts
@@ -1,14 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export async function getUserFromDbByEmail(email:any) {
-  const user = await prisma.user.findUnique({
-    where:{
-     email:email
-    }
-  })
-  return user
+export async function getUserFromDbByEmail(email: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    return user;
+  } catch (error) {
+    console.error("Error fetching user from database:", error);
+    throw error; // Propagate error to the calling function
+  } finally {
+    await prisma.$disconnect(); // Ensure PrismaClient is properly disconnected after the query
+  }
 }
-
-
